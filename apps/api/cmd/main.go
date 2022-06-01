64c26cd22a33fe2ab3955b7fdb44b7b32de51392
Christian Bartelt <christian.bartelt@uninow.de>
2022-06-01 08:51:25 +0200

    wip: ci for website and api

--- apps/api/cmd/main.go
+++ apps/api/cmd/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, Worldssss!")
+		return c.String(http.StatusOK, "Hello, Worldssssss!")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
