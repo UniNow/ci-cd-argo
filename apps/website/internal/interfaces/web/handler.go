10e5850ef59dd1602b906579d7caeb7f32efb800
Christian Bartelt <christian.bartelt@uninow.de>
2022-06-01 13:52:58 +0200

    wip: ci for website and api

--- apps/website/internal/interfaces/web/handler.go
+++ apps/website/internal/interfaces/web/handler.go
@@ -1,1 +1,13 @@
 package web
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+type Handler struct {
+}
+
+func (h Handler) ShowHome(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
--- apps/website/internal/interfaces/web/routes.go
+++ apps/website/internal/interfaces/web/routes.go
@@ -1,1 +1,11 @@
 package web
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+func RegisterRoutes(e *echo.Echo) error {
+	h := Handler{}
+	e.GET("/", h.ShowHome)
+	return nil
+}
