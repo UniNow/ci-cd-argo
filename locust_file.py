243d860ba2e67e38a624f800c2845013ac133e84
Christian Bartelt <christian.bartelt@uninow.de>
2022-06-02 16:15:23 +0200

    feat: e2e tests

--- locust_file.py
+++ locust_file.py
@@ -1,7 +1,10 @@
 from locust import HttpUser, task
 
-class HelloWorldUser(HttpUser):
+
+class CICDDemoUser(HttpUser):
     @task
     def hello_world(self):
         self.client.get("/")
+        self.client.get("/contact")
+        self.client.get("/users")
         self.client.get("/api/users")
