7804b5eb4ce2f1cb440d6e1caaee26d28f8718aa
Christian Bartelt <christian.bartelt@uninow.de>
2022-06-02 12:05:34 +0200

    feat: add api tests

--- apps/api/cmd/api/main.go
+++ apps/api/cmd/api/main.go
@@ -2,20 +2,11 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
-	"net/http"
+	"main/internal/interfaces/rest"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, Worldss!")
-	})
-	e.GET("/api", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-
-	e.GET("/health", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, Health!")
-	})
+	rest.RegisterRoutes(e)
 	e.Logger.Fatal(e.Start(":1323"))
 }
